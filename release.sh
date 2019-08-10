c4977f0855f12b9f02fc8ca9faeda9e76a14f3ad
Mehdi Hasan <mhasan@omicronlab.com>
2019-08-10 19:24:37 +0600

    Update release config

--- release.sh
+++ release.sh
@@ -6,5 +6,7 @@ export GOPROXY=https://gocenter.io
 # git tag -a v0.1.0 -m "First release"
 # git push origin v0.1.0
 
+# For dry run:
+# goreleaser --snapshot --skip-publish --rm-dist
 
 goreleaser release --rm-dist
